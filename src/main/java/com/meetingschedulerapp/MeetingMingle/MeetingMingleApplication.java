cca851ed65a9fb5af2e0214efcfe9c66f3513986
HumaidIqbal <humaidiqbal86@gmail.com>
2023-08-14 14:22:40 +0500

    first commit

---
+++ src/main/java/com/meetingschedulerapp/MeetingMingle/MeetingMingleApplication.java
@@ -0,0 +1,13 @@
+package com.meetingschedulerapp.MeetingMingle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingMingleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingMingleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meetingschedulerapp/MeetingMingle/MeetingMingleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meetingschedulerapp.MeetingMingle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingMingleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
